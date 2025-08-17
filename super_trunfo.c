1affb925f6fe4e2041379f81b34779a6e23789bd
VictorDerrihu <victorderrihu@gmail.com>
2025-08-17 12:26:52 -0300

    desafio nivel aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -11,6 +11,8 @@ int main(){
   float area;
   float pib;
   int turistic_points;
+  float population_density;
+  float pib_per_capita;
 
   // Declaração das variáveis dos valores utilizados na carta 2;
 
@@ -21,6 +23,8 @@ int main(){
   float area_2;
   float pib_2;
   int turistic_points_2;
+  float population_density_2;
+  float pib_per_capita_2;
 
   // Cadastro da Carta 1
 
@@ -47,7 +51,11 @@ int main(){
   printf("Qual é o número de pontos túristicos da cidade?\n");
   scanf("%d",&turistic_points);
 
-  printf("\nCarta 1:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d\n",state,cod,city_name,population,area,pib,turistic_points);
+  population_density = (float) population / area;
+  pib_per_capita = pib / (float) population;
+
+
+  printf("\nCarta 1:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n",state,cod,city_name,population,area,pib,turistic_points,population_density,pib_per_capita);
 
   // Cadastro para a Carta 2
   printf("\n\nAgora nós vamos cadastrar a Carta 2 :\n");
@@ -73,7 +81,10 @@ int main(){
   printf("Qual é o número de pontos túristicos da cidade?\n");
   scanf("%d",&turistic_points_2);
 
-  printf("\nCarta 2:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d\n",state_2,cod_2,city_name_2,population_2,area_2,pib_2,turistic_points_2);
+  population_density_2 = (float) population_2 / area_2;
+  pib_per_capita_2 = pib_2  / (float) population_2;
+
+  printf("\nCarta 2:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n",state_2,cod_2,city_name_2,population_2,area_2,pib_2,turistic_points_2,population_density_2,pib_per_capita_2);
   
    return 0;
 }
